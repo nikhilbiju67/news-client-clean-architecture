c3e5d0934f7b76037aecca7f4cfa7cc0682e164b
nikhilbiju67 <nikhilbiju67@gmail.com>
2023-09-10 01:36:45 +0530

    feat: add models

---
+++ news_api/app/src/main/java/com/kotlin/news_api/data/model/Source.kt
@@ -0,0 +1,11 @@
+package com.kotlin.news_api.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Source(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("name")
+    val name: String
+)
