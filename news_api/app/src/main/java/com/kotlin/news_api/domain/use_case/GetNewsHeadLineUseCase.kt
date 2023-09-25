89bab925fb181e1dc7ddd1baad84531e6e3f68fe
nikhilbiju67 <nikhilbiju67@gmail.com>
2023-09-25 13:32:01 +0530

    news fragment

--- news_api/app/src/main/java/com/kotlin/news_api/domain/use_case/GetNewsHeadLineUseCase.kt
+++ news_api/app/src/main/java/com/kotlin/news_api/domain/use_case/GetNewsHeadLineUseCase.kt
@@ -1,6 +1,12 @@
 package com.kotlin.news_api.domain.use_case
 
+import com.kotlin.news_api.data.model.APIResponse
+import com.kotlin.news_api.data.utils.Resource
 import com.kotlin.news_api.domain.repository.NewsRepository
 
 class GetNewsHeadLineUseCase(private val newsRepository: NewsRepository) {
+    suspend fun execute(country:String ,page:Int): Resource<APIResponse> {
+        return newsRepository.getNewsHeadlines(country,page);
+    }
+
 }
---
+++ news_api/build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+    kotlin("kapt") version "1.9.10"
+
+
+}
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        var nav_version = "2.7.2";
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+
+    }
+}
